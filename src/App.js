b7b21dc9f392b3e87311ef2df824ec3c9a68a147
Jaco Otto <jaco.otto@santander.co.uk>
2022-07-15 15:27:22 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
+import {useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  
+const timeInMinutes = new URLSearchParams(window.location.search).get('time');
+
+const[timer,setTimer] = useState(0)
+
+function addTextToBody() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      You have selected {timer} minutes of timer
+      {timer}
+    </div>
+    )
+}
+
+
+
+useEffect(()=> {
+  if (timeInMinutes) {
+    addTextToBody()
+  }
+},[])
+
+useEffect(() => {
+  if (timeInMinutes) {
+    setTimer(timeInMinutes) 
+    CountDown()
+  } 
+},[])
+
+const CountDown = (timeInMinutes) => {
+  const ticktock = setInterval(
+    () => {setTimer(prev => prev--)
+    if (timer == 0) {
+      clearInterval(ticktock)
+    }},
+    1000
+  )
+} 
+ 
+return (
+    <div className="App">    <h1>Timings!</h1>
+
+      {!timer ?
+      <form action=""
+        id="time-form">
+        <label for="time">How long do you want? (Minutes)</label>
+
+        <input type="number"
+            name="time"
+            id="time" />
+
+        <button type="submit">Submit</button>
+    </form>
+  :addTextToBody()  
+  }
+
     </div>
   );
 }
